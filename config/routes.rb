387a213760be8a1577fd1c1fbc9d7a9b1488dd84
Enock Ngetich <ngetichenock32@gmail.com>
2023-08-03 11:21:55 +0300

    Migrates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :applications
+  resources :jobs
+  resources :job_seekers
+  resources :employers
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
