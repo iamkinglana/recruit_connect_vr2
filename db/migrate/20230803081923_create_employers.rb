59e72ff5d7adb527ac64682374b36c49d7a3c1ca
Enock Ngetich <ngetichenock32@gmail.com>
2023-08-03 11:39:37 +0300

    Migrates Updates

--- db/migrate/20230803081923_create_employers.rb
+++ db/migrate/20230803081923_create_employers.rb
@@ -2,7 +2,19 @@ class CreateEmployers < ActiveRecord::Migration[7.0]
   def change
     create_table :employers do |t|
 
-      t.timestamps
+
+      t.belongs_to :employer, null: false, foreign_key: true
+      t.string :job_title
+      t.string :job_description
+      t.string :job_location
+      t.string :job_category
+      t.string :job_level
+      t.string :job_skills
+      t.string :job_qualifications
+      t.integer :salary_highest
+      t.integer :salary_lowest
+      t.datetime :application_deadline
+
     end
   end
 end
